38f12be8f242e75417f5d1c8bdd034a594f59bb3
zzkskys <2336276765@qq.com>
2020-04-10 14:36:58 +0800

    废弃 EditorUsersResource.java 因为 User 使用的是业务系统而不是 act_id_user

--- src/main/java/com/qunchuang/modeler/rest/GroupController.java
+++ src/main/java/com/qunchuang/modeler/rest/GroupController.java
@@ -1,9 +1,9 @@
 package com.qunchuang.modeler.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.flowable.idm.api.Group;
+import org.flowable.idm.api.IdmIdentityService;
+import org.flowable.ui.common.model.GroupRepresentation;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,12 +12,52 @@ import java.util.List;
 @RequestMapping("/groups")
 public class GroupController {
 
-    @GetMapping("/{groupId}")
+    private final IdmIdentityService idmIdentityService;
+
+    public GroupController(IdmIdentityService idmIdentityService) {
+        this.idmIdentityService = idmIdentityService;
+    }
+
+    /**
+     * 查询某组下的用户
+     */
+    @GetMapping("/{groupId}/users")
     public List<User> findUsersByGroupId(@PathVariable String groupId) {
         List<User> users = new ArrayList<>();
         users.add(new User("张三"));
         users.add(new User("李四"));
 
         return users;
+    }
+
+    /**
+     * 查询特定的分组
+     */
+    @GetMapping
+    public List<GroupRepresentation> getGroups(String search,
+                                               @RequestParam(defaultValue = "10") int size) {
+        List<GroupRepresentation> result = new ArrayList<>();
+        if (search == null || search.isEmpty()) {
+            List<Group> groups = idmIdentityService
+                    .createGroupQuery()
+                    .listPage(0, size);
+
+            for (Group group : groups) {
+                result.add(new GroupRepresentation((group)));
+            }
+        } else {
+            search = search.trim();
+            String sql = "select * from ACT_ID_GROUP where NAME_ like #{name} limit 10";
+            search = "%" + search + "%";
+            List<Group> groups = idmIdentityService
+                    .createNativeGroupQuery()
+                    .sql(sql)
+                    .parameter("name", search)
+                    .list();
+            for (Group group : groups) {
+                result.add(new GroupRepresentation(group));
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/qunchuang/modeler/rest/UserController.java
+++ src/main/java/com/qunchuang/modeler/rest/UserController.java
@@ -15,6 +15,9 @@ import java.util.UUID;
 @RequestMapping("/users")
 public class UserController {
 
+    /**
+     * 查询人员并分页
+     */
     @GetMapping("/page")
     public PageImpl<User> users(@RequestParam(value = "organUnitIds", required = false) List<String> organUnitIds,
                                 Role role,
@@ -33,6 +36,9 @@ public class UserController {
     }
 
 
+    /**
+     * 根据机构和角色查询人员
+     */
     @GetMapping("/by-organ-unit")
     public List<User> findByOrganUnit(String organUnitId, Role role) {
         List<User> users = new ArrayList<>();
@@ -43,7 +49,18 @@ public class UserController {
         return users;
     }
 
+    /**
+     * 查询人员并设置大小
+     */
+    @GetMapping
+    public List<User> users(String search, int size) {
+        List<User> users = new ArrayList<>();
+        users.add(new User("张三"));
+        users.add(new User("李四"));
+        users.add(new User("王五"));
 
+        return users;
+    }
 }
 
 
